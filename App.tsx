dbbf3e9c685ee97d5328215c7feed442afd9ab00
Hamam <zero.saturnus@gmail.com>
2023-01-12 15:10:53 +0700

    create UI login

--- App.tsx
+++ App.tsx
@@ -1,120 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button} from '@rneui/themed';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const loginWithFacebook = () => {};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const loginWithGoogle = () => {};
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        padding: 16,
+        justifyContent: 'center',
+      }}>
+      <Text style={{fontWeight: 'bold', marginBottom: 32, fontSize: 20}}>
+        Social Login Using AWS Amplify
+      </Text>
+      <Button
+        title="Login With Facebook"
+        icon={{
+          name: 'facebook',
+          type: 'font-awesome',
+          size: 14,
+          color: 'white',
+        }}
+        // icon={require('./src/assets/facebook.png')}
+        iconContainerStyle={{marginRight: 16}}
+        iconPosition={'left'}
+        titleStyle={{
+          color: 'white',
+        }}
+        buttonStyle={{
+          backgroundColor: 'rgba(90, 154, 230, 1)',
+          borderColor: 'transparent',
+          borderWidth: 0,
+          borderRadius: 5,
+          justifyContent: 'flex-start',
+          width: 200,
+        }}
+        onPress={loginWithFacebook}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+      <Button
+        title="Login With Google"
+        iconPosition={'left'}
+        icon={{
+          name: 'google',
+          type: 'font-awesome',
+          size: 14,
+          color: 'white',
+          // color: '#0F9D58',
+        }}
+        // icon={require('./src/assets/facebook.png')}
+        iconContainerStyle={{marginRight: 16}}
+        titleStyle={{
+          color: 'white',
+        }}
+        buttonStyle={{
+          backgroundColor: '#0F9D58',
+          borderColor: 'transparent',
+          borderRadius: 5,
+          // justifyContent: 'flex-start',
+          justifyContent: 'flex-start',
+          width: 200,
+        }}
+        containerStyle={{
+          marginTop: 16,
+        }}
+        onPress={loginWithGoogle}
+      />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
